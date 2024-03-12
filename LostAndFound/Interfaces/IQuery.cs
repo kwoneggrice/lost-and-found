cb78a918389905ecc059ec15d19e677228b93e6e
kwoneggrice <kwjddls1919@gmail.com>
2024-03-12 15:00:26 +0900

    Feat: 쿼리 메소드 인터페이스 구현

---
+++ LostAndFound/Interfaces/IQuery.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LostAndFound.Interfaces
+{
+	public interface IQuery<T>
+	{
+		#region METHODS
+
+		// 전체 조회
+		List<T> GetAll();
+
+		// 선택 조회
+		T GetDetail(long id);
+
+		// CREATE
+		void Create(T entity);
+
+		// Update
+		void Update(T entity);
+
+		// Delete
+		void Delete(long id);
+
+		#endregion
+	}
+}
