98161ed894082d9c41a05bf969e6570acbf59ae7
kwoneggrice <kwjddls1919@gmail.com>
2024-03-12 15:10:07 +0900

    Comment: 주석 추가

--- LostAndFound/Interfaces/IQuery.cs
+++ LostAndFound/Interfaces/IQuery.cs
@@ -10,19 +10,35 @@ namespace LostAndFound.Interfaces
 	{
 		#region METHODS
 
-		// 전체 조회
+		/// <summary>
+		/// 데이터베이스 테이블에서 전체 데이터 조회
+		/// </summary>
+		/// <returns></returns>
 		List<T> GetAll();
 
-		// 선택 조회
+		/// <summary>
+		/// 데이터베이스 테이블에서 특정 데이터 조회
+		/// </summary>
+		/// <param name="id">조회할 행의 id</param>
+		/// <returns></returns>
 		T GetDetail(long id);
 
-		// CREATE
+		/// <summary>
+		/// 데이터베이스 테이블의 행 삽입
+		/// </summary>
+		/// <param name="entity">삽입할 행의 entity</param>
 		void Create(T entity);
 
-		// Update
+		/// <summary>
+		/// 데이터베이스 테이블의 행 수정
+		/// </summary>
+		/// <param name="entity">수정된 행의 entity</param>
 		void Update(T entity);
 
-		// Delete
+		/// <summary>
+		/// 데이터베이스 테이블의 행 삭제
+		/// </summary>
+		/// <param name="id">삭제할 행의 id</param>
 		void Delete(long id);
 
 		#endregion
