10faef7008a9637937b05835f54badc8aef3e4e9
kwoneggrice <kwjddls1919@gmail.com>
2024-03-15 21:29:03 +0900

    Design: Login 폼 UI 구현

---
+++ LostAndFound/Forms/FormLogin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LostAndFound.Forms
+{
+	public partial class FormLogin : Form
+	{
+		#region FEILDES
+
+		private readonly FormSignup _formSignup;
+
+		#endregion
+
+		#region CONSTRUCTOR
+
+		public FormLogin(FormSignup formSignup)
+		{
+			InitializeComponent();
+			_formSignup = formSignup;
+		}
+
+		#endregion
+
+		private void btnSignup_Click(object sender, EventArgs e)
+		{
+			_formSignup.ShowDialog();
+		}
+	}
+}
