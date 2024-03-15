b8ab5803ad6d1a833ec0b52927549f30540254eb
kwoneggrice <kwjddls1919@gmail.com>
2024-03-15 21:13:20 +0900

    Feat: 회원가입 및 로그인 시 패스워드 단방향 해싱 구현

---
+++ LostAndFound/Utils/PasswordUtil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LostAndFound.Utils
+{
+	public class PasswordUtil
+	{
+		public static string HasingPassword(string password)
+		{
+			using (SHA256 hash = SHA256.Create())
+			{
+				StringBuilder sb = new StringBuilder();
+
+				byte[] bytes = hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+
+				for (int i = 0; i < bytes.Length; i++)
+				{
+					sb.Append(bytes[i].ToString("x2"));
+				}
+
+				return sb.ToString();
+			}
+		}
+	}
+}
